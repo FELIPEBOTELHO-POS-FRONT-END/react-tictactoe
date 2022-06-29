3a4b9106bfb500e0734786b7c7ffeec6dbde9c87
Felipe Botelho <felipebotelhofelipe@hotmail.com>
2022-06-29 08:49:03 -0300

    tictactoe redux

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import { Provider } from "react-redux";
+import { store } from "./store";
+import TicTacToe from "./TicTacToe";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <TicTacToe />
+    </Provider>
+  );
+}
+
+export default App;
